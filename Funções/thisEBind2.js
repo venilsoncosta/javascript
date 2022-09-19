6c3008d9e598a09708979621d534ce37f21a6afe
Venilson Neves <38733697+venilsoncosta@users.noreply.github.com>
2022-09-19 07:12:17 -0300

    Atualizado

---
+++ Funções/thisEBind2.js
@@ -0,0 +1,12 @@
+function Pessoa() {
+    this.idade = 0
+
+    const self = this
+    setInterval(function () {
+        self.idade++
+        console.log(self.idade)
+
+    }, 2000)
+}
+
+new Pessoa
